9575384de7dfaf1c56a19e72df97eb2f2af6b511
Christopher Ritsen <chris.ritsen@gmail.com>
2022-02-15 18:08:05 -0500

    Set minimum python version

--- netaudio/__init__.py
+++ netaudio/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 from .dante.browser import DanteBrowser
 from .dante.channel import DanteChannel
 from .dante.control import DanteControl
@@ -9,4 +11,5 @@ __author__ = 'Chris Ritsen'
 __maintainer__ = 'Chris Ritsen <chris.ritsen@gmail.com>'
 __version__ = '0.0.0'
 
-__all__ = []
+if sys.version_info <= (3, 9):
+    raise ImportError('Python version > 3.9 required.')
