70db6898423ee4e64fd5b51dd67aee2b3b2cb7b2
Christopher Ritsen <chris.ritsen@gmail.com>
2022-02-17 15:05:24 -0500

    Implement new CLI front-end with cleo

--- netaudio/__init__.py
+++ netaudio/__init__.py
@@ -5,6 +5,14 @@ from .dante.device import DanteDevice
 from .dante.multicast import DanteMulticast
 from .dante.subscription import DanteSubscription
 
+#  from .commands.device import DeviceCommand
+#  from .commands.list import ListCommand
+#  from .commands.subscription.add import SubscriptionAddCommand
+#  from .commands.subscription.remove import SubscriptionRemoveCommand
+#  from .commands.subscription import SubscriptionCommand
+
+from .console.application import main
+
 __author__ = 'Chris Ritsen'
 __maintainer__ = 'Chris Ritsen <chris.ritsen@gmail.com>'
 __version__ = '0.0.0'
---
+++ netaudio/console/commands/__init__.py
@@ -0,0 +1,6 @@
+
+from .device import DeviceCommand
+from .list import ListCommand
+from .subscription.add import SubscriptionAddCommand
+from .subscription.remove import SubscriptionRemoveCommand
+from .subscription import SubscriptionCommand
