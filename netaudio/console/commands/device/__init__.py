ed7f17c5e87d8350ed0b24ab5108ae975b08bea4
Christopher Ritsen <chris.ritsen@gmail.com>
2022-02-21 18:56:17 -0500

    Fix broken features

--- netaudio/console/commands/device/__init__.py
+++ netaudio/console/commands/device/__init__.py
@@ -13,40 +13,5 @@ class DeviceCommand(Command):
     description = "Control devices"
     commands = [DeviceListCommand()]
 
-    options = [
-        option("identify", None, "Identify the device by flashing an LED"),
-        option("name", None, "Specify a device name", flag=False),
-    ]
-
-    async def identify(self):
-        dante_browser = DanteBrowser(mdns_timeout=1.5)
-        devices = await dante_browser.get_devices()
-        name = None
-
-        if self.option("name"):
-            name = self.option("name")
-
-        else:
-            for _, device in devices.items():
-                await device.get_controls()
-
-            device_names = sorted(
-                list({k: v.name for k, v in devices.items()}.values())
-            )
-            name = self.choice("Select a device", device_names, None)
-
-        if not name:
-            return
-
-        for _, device in devices.items():
-            if self.option("name"):
-                await device.get_controls()
-
-            if device.name == name:
-                await device.identify()
-
     def handle(self):
-        if self.option("identify"):
-            asyncio.run(self.identify())
-        else:
-            return self.call("help", self._config.name)
+        return self.call("help", self._config.name)
