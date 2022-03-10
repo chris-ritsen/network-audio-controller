a9cece2fc351a302aa210ad1367a9708dea5139f
Christopher Ritsen <chris.ritsen@gmail.com>
2022-03-10 00:24:59 -0500

    Add experimental server feature

--- netaudio/console/__init__.py
+++ netaudio/console/__init__.py
@@ -1,6 +1,7 @@
 from .commands.channel import ChannelCommand
 from .commands.config import ConfigCommand
 from .commands.device import DeviceCommand
+from .commands.server import ServerCommand
 from .commands.subscription import SubscriptionCommand
 from .commands.subscription._add import SubscriptionAddCommand
 from .commands.subscription._remove import SubscriptionRemoveCommand
