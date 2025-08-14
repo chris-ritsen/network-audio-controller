f2a2a9b3472c6400646291180244a15a5e53e307
Gonzalo Ruiz <rgon@rgon.es>
2025-08-14 19:11:09 +0200

    refactor: redo command definition according to how poetry (creators of cleo) do it

--- netaudio/console/application.py
+++ netaudio/console/application.py
@@ -1,13 +1,19 @@
 from cleo.application import Application
+from cleo.commands.command import Command
 from netaudio import version
 
-from netaudio.console.commands import (
-    ChannelCommand,
-    ConfigCommand,
-    DeviceCommand,
-    ServerCommand,
-    SubscriptionCommand,
-)
+from typing import Callable
+
+COMMANDS = [
+    "config",
+    "device list",
+    "channel list",
+    "server http",
+    "server mdns",
+    "subscription add",
+    "subscription remove",
+    "subscription list"
+]
 
 # Fix Windows issue, See: https://stackoverflow.com/q/58718659/
 try:
@@ -18,22 +24,24 @@ except ImportError:  # If SIGPIPE is not available (win32),
     pass  # we don't have to do anything to ignore it.
 
 
-def main() -> int:
-    commands = [
-        ChannelCommand,
-        ConfigCommand,
-        DeviceCommand,
-        ServerCommand,
-        SubscriptionCommand,
-    ]
+def load_command(name: str) -> Callable[[], Command]:
+    from importlib import import_module
 
+    def _load() -> Command:
+        words = name.split(" ")
+        module = import_module("netaudio.console.commands." + ".".join(words))
+        command_class = getattr(module, "".join(c.title() for c in words) + "Command")
+        command: Command = command_class()
+        return command
+
+    return _load
+
+
+def main() -> int:
     application = Application("netaudio", version.version)
 
-    for command in commands:
-        application.add(command())
-
-        for subcommand in command.commands if command.commands else []:
-            application.add(subcommand)
+    for command in COMMANDS:
+        application.add(load_command(command)())
 
     return application.run()
 
