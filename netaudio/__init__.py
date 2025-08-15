1f435aecbad0354a6b49ea81ae946948d6ed0c8b
Gonzalo Ruiz <rgon@rgon.es>
2025-08-15 21:51:17 +0200

    chore: let poetry handle python version compatibility

--- netaudio/__init__.py
+++ netaudio/__init__.py
@@ -1,18 +1,5 @@
 import sys
 
-# from .dante.browser import DanteBrowser
-# from .dante.channel import DanteChannel
-# from .dante.control import DanteControl
-# from .dante.device import DanteDevice
-# from .dante.multicast import DanteMulticast
-# from .dante.subscription import DanteSubscription
-
-__author__ = "Chris Ritsen"
-__maintainer__ = "Chris Ritsen <chris.ritsen@gmail.com>"
-
-if sys.version_info <= (3, 9):
-    raise ImportError("Python version > 3.9 required.")
-
 def main():
     from netaudio.commands import run_cli
     import signal
