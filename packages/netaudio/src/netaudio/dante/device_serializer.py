85a9db8b5f970d05d1a664ceb07e595b32b9dedb
Christopher Ritsen <chris.ritsen@gmail.com>
2026-09-02 09:12:19 -0400

    Daemon HTTP device records carry legacy key aliases for fleet consumers

--- packages/netaudio/src/netaudio/dante/device_serializer.py
+++ packages/netaudio/src/netaudio/dante/device_serializer.py
@@ -134,6 +134,14 @@ def device_json_field_name(field_name: str) -> str:
     return DEVICE_JSON_FIELD_NAMES.get(field_name, field_name)
 
 
+def with_legacy_field_names(record: dict) -> dict:
+    aliased = dict(record)
+    for legacy_name, json_field_name in DEVICE_JSON_FIELD_NAMES.items():
+        if json_field_name in record and legacy_name not in record:
+            aliased[legacy_name] = record[json_field_name]
+    return {key: aliased[key] for key in sorted(aliased)}
+
+
 class DanteDeviceSerializer:
     @staticmethod
     def to_json(device):
